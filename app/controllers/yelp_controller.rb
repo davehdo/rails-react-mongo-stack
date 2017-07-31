81dcfc2d0934e46597dc27d45ea69f4e9b7ec4a5
Laura Do <misslaurado@gmail.com>
2017-07-31 13:51:07 -0400

    remove pry

--- app/controllers/yelp_controller.rb
+++ app/controllers/yelp_controller.rb
@@ -19,7 +19,6 @@ class YelpController < ApplicationController
     response = HTTP.auth("Bearer #{ENV["YOUR_TOKEN"]}").get(url, params: query)
 
     if response.status == 200
-      binding.pry
       body = JSON.parse(response.body)
       render json: body
     else

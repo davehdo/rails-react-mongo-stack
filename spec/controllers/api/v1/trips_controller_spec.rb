f93cca6599bed490d795cfca90c682c8c50aab4d
Laura Do <misslaurado@gmail.com>
2017-08-01 11:19:02 -0400

    add controller tests

--- spec/controllers/api/v1/trips_controller_spec.rb
+++ spec/controllers/api/v1/trips_controller_spec.rb
@@ -56,9 +56,50 @@ RSpec.describe Api::V1::TripsController, type: :controller do
     end
 
     describe "POST#create" do
+      it "should create a trip with the correct info if the user is signed in" do
+        user = FactoryGirl.create(:user)
+
+        post_json = {
+          name: "Boston",
+          city: "Boston",
+          state: "MA",
+          user_id: user.id
+        }.to_json
+
+        sign_in user
+
+        expect { post(:create, body: post_json) }.to change { Trip.count }.by 1
+        post(:create, body: post_json)
+
+        returned_json = JSON.parse(response.body)
+        expect(response.status).to eq 200
+
+        expect(returned_json).to be_kind_of(Hash)
+        expect(returned_json).to_not be_kind_of(Array)
+        expect(returned_json["trip"]["name"]).to eq "Boston"
+        expect(returned_json["trip"]["city"]).to eq "Boston"
+        expect(returned_json["trip"]["state"]).to eq "MA"
+      end
     end
 
     describe "DELETE#destroy" do
+      it "should delete a trip and return the same trip" do
+        user = FactoryGirl.create(:user)
+        trip1 = FactoryGirl.create(:trip, user: user)
+
+        sign_in user
+
+        expect { delete(:destroy, params: {id: trip1.id}) }.to change { Trip.count }.by -1
+
+        returned_json = JSON.parse(response.body)
+        expect(response.status).to eq 200
+
+        expect(returned_json).to be_kind_of(Hash)
+        expect(returned_json).to_not be_kind_of(Array)
+        expect(returned_json["trip"]["name"]).to eq trip1.name
+        expect(returned_json["trip"]["city"]).to eq trip1.city
+        expect(returned_json["trip"]["state"]).to eq trip1.state
+      end
     end
 
 end

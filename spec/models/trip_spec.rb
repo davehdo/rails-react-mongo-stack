0bf830c5c31d2b70a5fd741b3f4ec0adba57d0f6
Laura Do <misslaurado@gmail.com>
2017-10-18 20:31:07 -0400

    add unit test for trip

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -6,4 +6,12 @@ describe Trip, type: :model do
 
   it { should have_valid(:city).when('Boston', 'Minneapolis') }
   it { should_not have_valid(:city).when(nil, '') }
+
+  describe '#address' do
+    let(:trip) { create(:trip) }
+
+    it 'returns the formatted city and state' do
+      expect(trip.address).to eq "#{trip.city}, #{trip.state}"
+    end
+  end
 end

0ada7cdafe59074d42f48ec6abdff9ca2813ffaf
David Do <daviddo@Davids-MacBook-Pro.local>
2017-12-26 21:32:08 -0500

    removed migrations

--- db/migrate/20170806021054_add_lat_to_trip.rb
+++
@@ -1,6 +0,0 @@
-class AddLatToTrip < ActiveRecord::Migration[5.1]
-  def change
-    add_column :trips, :lat, :float
-    add_column :trips, :lon, :float
-  end
-end
--- db/migrate/20170806124747_add_lat_to_restaurant.rb
+++
@@ -1,6 +0,0 @@
-class AddLatToRestaurant < ActiveRecord::Migration[5.1]
-  def change
-    add_column :restaurants, :lat, :float
-    add_column :restaurants, :lon, :float
-  end
-end

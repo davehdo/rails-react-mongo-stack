b1b532414dd6644675b90fd818fb4484ffbc27da
David Do <daviddo@Davids-MacBook-Pro.local>
2018-01-03 15:42:08 -0500

    cleaned up footers and devise forms

--- react/src/components/Footer.js
+++ react/src/components/Footer.js
@@ -4,13 +4,6 @@ const Footer = props => {
 
   return(
     <div>
-      <hr/>
-      <div className="github">
-        <i className="fa fa-github" aria-hidden="true"></i>
-        <p>github.com/laurado</p><br/>
-        <i className="fa fa-linkedin-square" aria-hidden="true"></i>
-        <p>linkedin.com/in/laura-do</p><br/>
-      </div>
     </div>
   )
 }
